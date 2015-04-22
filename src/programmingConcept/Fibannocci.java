00bd87e0f1360d16a84f902cc4759f6b2f48ce38
nasneem <s.nasneem@mcwaneservices.com>
2015-04-22 17:57:36 +0530

    Sum of even Fibanocci numbers till 4million implemented.PHI value is printed

--- src/programmingConcept/Fibannocci.java
+++ src/programmingConcept/Fibannocci.java
@@ -13,16 +13,28 @@ public class Fibannocci {
         long fib1=2;
         long fibTemp = 0;
         long fibResult=2L;
+        float fibdiv;
+        long fibprev=2;
+        int count=0;
         System.out.println("The sum of even fibanocci from 0 to limit(4000000000) is :" + fib0 +" "+fib1);
+         fibdiv = (float)fib0/(float)fib1;
+        System.out.println("1Division value:"+fibdiv);
+
                 while(fibTemp <= extremeLimit) {
 
                     fibTemp = fib0 + fib1;
+
+                    if(fibTemp<10000){
+                       fibdiv = (float)fibprev/(float)fibTemp;
+                    System.out.println("Division value:"+fibdiv);
+                    }
                     fib0 = fib1;
                     fib1 = fibTemp;
 
                     if(fibTemp%2==0) {
                         fibResult += fibTemp;
                     }
+                    fibprev=fibTemp;
                 }
         System.out.println("The sum of even fibanocci from 0 to limit(4000000000) is :" + fibResult);
          }
