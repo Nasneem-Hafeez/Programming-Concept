b6788518e1bb1622487cd1738a48984f7c2974fa
nasneem <s.nasneem@mcwaneservices.com>
2015-04-23 18:30:34 +0530

    PrimeFactors completed.

--- src/programmingConcept/PrimeFactors.java
+++ src/programmingConcept/PrimeFactors.java
@@ -1,84 +1,34 @@
+
 package programmingConcept;
-import java.util.HashSet;
-import java.util.Random;
-import java.util.Scanner;
-import java.util.Set;
 
-
-
-/**
- * Created by sudhir on 23/4/15.
- */
-public class PrimeFactors {
-
-    public static void main(String[] args) {
-
-        //new PrimeFactors ( ) .primeGenerators(50);
-          primeGenerators(10);
-       // System.out.println(new PrimeFactors().primeGenerators(50));
-    }
-
-
-    private static void primeGenerators(long number) {
-
-        HashSet primefactors = new HashSet();//        Set primefactors = new HashSet();
-        long genOfpn, primeNo = 0;
-        int flag = 0;
-        //outerloop:   while (number > 1) {
-        for (genOfpn = 2; genOfpn < number; genOfpn++) {
-            flag = 0;
-            for (int Checkpn = 2; Checkpn <=genOfpn; Checkpn++) {
-             //   System.out.println("> "+genOfpn+ "> " +Checkpn);
-                if ((genOfpn % Checkpn == 0)) {
-                    flag++;
-                    if(genOfpn==Checkpn)
-                    {
-                        primeNo = genOfpn;
-                        break;
-                    }
-
-
-
-                }
-
-
+public class PrimeFactors
+{
+    public boolean isPrime(long n)
+    {
+        for(int i=2; i<=n/2; i++){
+            if(n % i == 0){
+                return false;
             }
-
-               if (flag == 1) {
-                   System.out.println("Answer:"+primeNo);
-                    // primeNo = i;
-                    //System.out.println(primeNo);
-                    // if (number % genOfpn == 0) {
-                    //  primefactors.add(primeNo);
-                    // number = number / genOfpn;
-                    // continue outerloop;
-
-                }
-
-
-
         }
-
-
-
-        //}private
-
-
-//return primefactors;
+        return true;
     }
+    public static void main(String[] args)
+    {
+        PrimeFactors p = new PrimeFactors();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+        long inputNo = 600851475143L;
+        long divisor = 2;
+        while (1==1)
+        {
+            long tmp = inputNo / divisor;
+            if ( inputNo % tmp == 0 &&  p.isPrime(tmp) )
+            {
+                System.out.println("The largest prime factor is = " + tmp);
+                break;
+            }
+            divisor++;
+        }
+    }
 
 }
+
